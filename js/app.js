77092b52d58c4b4b4a2e470bd40a0e8adec9b1a7
Miki Kiki <krawczykmichal1@gmail.com>
2023-08-30 12:36:18 +0100

    set fonts and headers for mobile first and add clams with media q to make fonts bigger for biggers screens

--- js/app.js
+++ js/app.js
@@ -5,3 +5,32 @@ document.addEventListener("DOMContentLoaded", function () {
 	const currentYear = new Date().getFullYear();
 	yearElement.textContent = currentYear;
 });
+
+// Parallax to 3 sections
+
+// document.addEventListener("scroll", function () {
+// 	let sections = document.querySelectorAll(".parallax-section");
+
+// 	Array.prototype.forEach.call(sections, function (section) {
+// 		let windowScrollTop = window.scrollY,
+// 			elementOffsetTop = section.offsetTop,
+// 			windowHeight = window.innerHeight,
+// 			elementHeight = section.offsetHeight,
+// 			viewPortTop = windowScrollTop - elementOffsetTop + windowHeight,
+// 			viewPortBottom = windowScrollTop + windowHeight - elementOffsetTop,
+// 			backgroundPosition = 0;
+
+// 		// Check if the element is in the viewport
+// 		if (
+// 			(viewPortTop > 0 && viewPortTop < elementHeight) ||
+// 			(viewPortBottom > 0 && viewPortBottom < windowHeight)
+// 		) {
+// 			backgroundPosition = -(
+// 				(windowScrollTop - elementOffsetTop) *
+// 				parseFloat(section.getAttribute("data-speed"))
+// 			);
+// 		}
+
+// 		section.style.backgroundPosition = "center " + backgroundPosition + "px";
+// 	});
+// });
