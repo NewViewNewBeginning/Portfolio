9fb383fa31d13c46f1a02b6a79dae371586ff561
Miki Kiki <krawczykmichal1@gmail.com>
2023-09-04 22:15:01 +0100

    add slider for skills section

--- js/app.js
+++ js/app.js
@@ -62,3 +62,15 @@ checkWindowWidth();
 
 // Dodaj zdarzenie resize dla okna
 window.addEventListener("resize", checkWindowWidth);
+
+const root = document.documentElement;
+const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue(
+	"--marquee-elements-displayed"
+);
+const marqueeContent = document.querySelector("ul.marquee-content");
+
+root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+
+for (let i = 0; i < marqueeElementsDisplayed; i++) {
+	marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+}
