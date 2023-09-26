c6b3ce0d62963028ad017c669c4efe071396bf2a
Miki Kiki <krawczykmichal1@gmail.com>
2023-09-26 11:08:27 +0100

    update php

--- form_process.php
+++ form_process.php
@@ -1,64 +1,74 @@
 <?php
-// Check if the form was submitted
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+session_start();
 
-    // Sanitize input data
-    $name = htmlspecialchars(trim($_POST['name']));
-    $surname = htmlspecialchars(trim($_POST['surname']));
-    $subject = htmlspecialchars(trim($_POST['subject']));
-    $email = htmlspecialchars(trim($_POST['email']));
-    $message = htmlspecialchars(trim($_POST['message']));
-
-    // Initialize an array to hold error messages
-    $errors = [];
-
-    // Validate name
-    if (empty($name)) {
-        $errors[] = "Name is required.";
-    }
-
-    // Validate surname
-    if (empty($surname)) {
-        $errors[] = "Surname is required.";
-    }
-
-    // Validate subject
-    if (empty($subject)) {
-        $errors[] = "Subject is required.";
-    }
-
-    // Validate email
-    if (empty($email)) {
-        $errors[] = "Email is required.";
-    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $errors[] = "Invalid email format.";
-    }
-
-    // Validate message
-    if (empty($message)) {
-        $errors[] = "Message is required.";
-    }
-
-    // Check if there were validation errors
-    if (!empty($errors)) {
-        foreach ($errors as $error) {
-            echo $error . "<br>";
-        }
+// Function to check against any email injection attempts
+function IsInjected($str) {
+    $injections = array('(\n+)', '(\r+)', '(\t+)', '(%0A+)', '(%0D+)', '(%08+)', '(%09+)');
+    $inject = join('|', $injections);
+    $inject = "/$inject/i";
+    if (preg_match($inject, $str)) {
+        return true;
     } else {
-        // For simplicity, this code just prints the data.
-        // In a real application, you might want to email the data, save it to a database, etc.
-        echo "Name: $name<br>";
-        echo "Surname: $surname<br>";
-        echo "Subject: $subject<br>";
-        echo "Email: $email<br>";
-        echo "Message: $message<br>";
+        return false;
+    }
+}
 
-        // Optional: Send an email, save to a database, etc.
+// Function to validate email domain
+function domain_exists($email) {
+    list($user, $domain) = explode('@', $email);
+    return checkdnsrr($domain, 'MX');
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Collect and sanitize form input
+    $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+    $surname = filter_var($_POST['surname'], FILTER_SANITIZE_STRING);
+    $subject = filter_var($_POST['subject'], FILTER_SANITIZE_STRING);
+    $email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
+    $message = filter_var($_POST['message'], FILTER_SANITIZE_STRING);
+
+    // Form validation
+    $errors = [];
+    if (empty($name) || empty($surname) || empty($subject) || empty($email) || empty($message)) {
+        $errors[] = 'All fields are required.';
     }
 
-} else {
-    // Redirect to the form page if the form was not submitted
-    header("Location: index.html");
+    if ($name[0] == '@' || $name[0] == '!' || $name[0] == '#' || $name[0] == '$' || $name[0] == '%') {
+        $errors[] = 'Invalid character at the start of the name.';
+    }
+
+    if ($surname[0] == '@' || $surname[0] == '!' || $surname[0] == '#' || $surname[0] == '$' || $surname[0] == '%') {
+        $errors[] = 'Invalid character at the start of the surname.';
+    }
+
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors[] = 'Invalid email address.';
+    } elseif (IsInjected($email) || !domain_exists($email)) {
+        $errors[] = 'Invalid or non-existent email domain.';
+    }
+
+    if (count($errors) > 0) {
+        $_SESSION['message'] = implode('<br>', $errors);
+        header('Location: index.html');
+        exit();
+    }
+
+    // Prepare email
+    $recipient = 'michalkrawczyk666@wp.pl';
+    $headers = "From: $name $surname <$email>\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $body = "Name: $name $surname\nEmail: $email\nSubject: $subject\nMessage:\n$message";
+
+    // Send email
+    if (mail($recipient, $subject, $body, $headers)) {
+        $_SESSION['message'] = 'Thank you, your message has been sent!';
+    } else {
+        $_SESSION['message'] = 'Oops, something went wrong. Please try again.';
+    }
+
+    // Redirect back to the form page
+    header('Location: index.html');
     exit();
 }
+
 ?>
